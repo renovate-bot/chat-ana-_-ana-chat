e2cf44a5ffe32585e7d1778c69d1bbac089db59b
MisileLab <misileminecord@gmail.com>
2023-08-16 17:39:06 +0900

    hotfix cors
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,8 @@
 use std::{env::set_var, path::PathBuf};
 
-use axum::{Router, routing::{get, post}};
+use axum::{Router, routing::{get, post}, http::Method};
 use shuttle_secrets::SecretStore;
-use tower_http::services::{ServeFile, ServeDir};
+use tower_http::{services::{ServeFile, ServeDir}, cors::{Any, CorsLayer}};
 mod user;
 mod common;
 mod msg;
@@ -15,8 +15,11 @@ async fn axum(
 ) -> shuttle_axum::ShuttleAxum {
     let uri = secret_store.get("MONGO_URI").expect("MONGO_URI not found");
     set_var("MONGO_URI", uri);
+    let cors = CorsLayer::new()
+    .allow_methods([Method::GET, Method::POST])
+    .allow_origin(Any);
     let router =
-        Router::new().nest_service("/", ServeDir::new(static_folder).not_found_service(ServeFile::new("index.html")))
+        Router::new().layer(cors).nest_service("/", ServeDir::new(static_folder).not_found_service(ServeFile::new("index.html")))
         .route("/user/create", post(user::create_user))
         .route("/user/info", get(user::get_user_info))
         .route("/server/create", post(server::create_server))
